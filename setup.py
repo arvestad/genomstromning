7998d593eb9e6bb3faee3fcbe22c7c2c046f6f46
arvestad <arve@math.su.se>
2023-05-04 15:50:17 +0200

    Working version

--- setup.py
+++ setup.py
@@ -12,43 +12,32 @@ with open("README.md", "r") as fh:
         long_description += line
 
 
-with open('dnctree/version.py') as fh:
+with open('genomstromning/version.py') as fh:
     exec(fh.read())
 
-if sys.version_info.major < 3:
-    sys.exit('\n'
-             'Sorry, Python 2 is not supported\n'
-             'Did you run pip install dnctree?\n'
-             'Try \'pip3 install dnctree\'')
-
-elif sys.version_info.minor < 2:
-    sys.exit('\nSorry, Python < 3.2 is not supported\n')
-
 requirements = [
-    'alv',
-    'biopython>=1.70',
+    'matplotlib',
 ]
 
 setuptools.setup(
-    name="dnctree",
+    name="genomstromning",
     version=__version__,
     author="Lars Arvestad",
     author_email="arve@math.su.se",
-    description="A console-based sequence alignment viewer",
+    description="Generate figures showing student retention",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/arvestad/dnctree",
-    test_suite = "tests",
+    url="https://github.com/arvestad/genomstromning",
+    #    test_suite = "tests",
     packages=setuptools.find_packages(),
-    python_requires='>=3.5',    # I want to merge dictionaries easily. From 3.5, {**x, **y} merges dictionaries x and y
+    python_requires='>=3.5',    # Arbitrary choice...
     entry_points = {
-        'console_scripts': ['dnctree = dnctree.main:main']
+        'console_scripts': ['genomstromning = genomstromning.main:main']
         },
     install_requires=requirements,
-    classifiers=(
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
-        "Topic :: Scientific/Engineering :: Bio-Informatics",
-    ),
+    ],
 )
